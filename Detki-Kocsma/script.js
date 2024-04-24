c1850af87a5c4169d53cb205ce18b52d2be984f0
Partisan1 <benedekbarnabas.zsolt@gmail.com>
2024-04-24 13:20:14 +0200

    lista, js elkezdese

--- Detki-Kocsma/script.js
+++ Detki-Kocsma/script.js
@@ -1,1 +1,34 @@
-const honapok = document.querySelector("header h3")
+const honaps = document.querySelector("header h3")
+const napok = document.querySelector(".dates")
+const cimsor = document.querySelector("#prev, #next")
+
+const honapok = [
+    "Január",
+    "Február",
+    "Március",
+    "Április",
+    "Május",
+    "Június",
+    "Július",
+    "Augusztus",
+    "Szeptember",
+    "Október",
+    "December",
+
+];
+
+
+let date = new Date();
+let honap = date.getMonth();
+let ev = date.getFullYear();
+
+
+function render() {
+    const start = new Date(ev, honap, 1).getDay();
+    const endDate = new Date(ev, honap + 1, 0).getDate();
+    const end = new Date(ev, honap, endDate).getDay();
+    const endPrev = new Date(ev, honap, 0).getDate();
+
+
+    let datesHtml = "";
+}
